8f141c6b726f32b130cd2b24d29077ba67e1efc1
student <student@imac-16.local>
2018-12-12 11:39:52 +0800

    add keyBoard

--- TextFieldAndTableView/ViewController.swift
+++ TextFieldAndTableView/ViewController.swift
@@ -240,7 +240,33 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         studentsArry.append(student)
     }
 //--------------------------
-   
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name:UIResponder.keyboardWillShowNotification, object: view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name:UIResponder.keyboardWillHideNotification, object: view.window)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
+    @objc func keyboardWillShow(notification: Notification){
+        if let userInfor = notification.userInfo {
+            if let rect = userInfor[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect {
+                self.view.frame.origin.y = -rect.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification){
+        self.view.frame.origin.y = 0
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
 }
 
